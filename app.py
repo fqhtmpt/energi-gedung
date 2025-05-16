f65790180b5720a74cbe7f0c7e232d9271bcd2cc
Muhammad fiqih <124853878+fqhtmpt@users.noreply.github.com>
2025-05-16 19:07:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -196,8 +196,7 @@ st.markdown("""
     <div style='text-align: center; color: gray; font-size: 14px;'>
         📍 Wisma Indocement, Jl. Jend. Sudirman Kav. 70-71, Jakarta Selatan 12910<br>
         🔧 Aplikasi dibuat saat magang di PT. Serasi Tunggal Mandiri<br>
+        © Copyright by Muhammad Fiqih Tampati<br>
         © 2025 All rights reserved.<br>
-        © Copyright by Muhammad Fiqih Tampati
-        © 2025 All rights reserved.
     </div>
 """, unsafe_allow_html=True)
